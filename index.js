5749582e533305adae4cb858d3034618a38b6619
Tali Scheer <tali.scheer@gmail.com>
2018-11-06 21:00:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(arr, b) {
-  arr.push("b");
+  arr.push(b);
   return a;
 }
