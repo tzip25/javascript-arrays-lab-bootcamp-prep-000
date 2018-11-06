1407a42284f9cbcd1a312af075934c9852f94821
Tali Scheer <tali.scheer@gmail.com>
2018-11-06 21:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten() {
-  var kittensNew = [kittens, ..."broom"]
+  var kittensNew = [kittens, ...'broom']
   return kittensNew
 }
